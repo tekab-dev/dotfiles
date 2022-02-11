71ed5f3919a8e93fdc9e2d237c485a4898eb54b8
achrafabidatekab <89009763+achrafabidatekab@users.noreply.github.com>
2022-02-11 11:53:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $THEIA_WORKSPACE_ROOT
 echo "working" > /home/gitpod/echo.txt
+echo "working" > echo.txt
 
 cp $THEIA_WORKSPACE_ROOT/.devfactory.yml $THEIA_WORKSPACE_ROOT/.gitpod.yml
 cp $THEIA_WORKSPACE_ROOT/.devfactory.yml $THEIA_WORKSPACE_ROOT/.gitpod2.yml
