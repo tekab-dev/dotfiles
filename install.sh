188a410e007b97a8a1e0b26ec3644a57fa519e69
achrafabidatekab <achraf.abida@tekab.dev>
2022-02-11 10:59:53 +0000

    make executabe

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $THEIA_WORKSPACE_ROOT
-echo "working" > /home/gitpod/echo.txt
-echo "working" > echo.txt
+echo "working" > /home/gitpod/echo.txt1
+echo "working" > echo.txt2
 
 cp $THEIA_WORKSPACE_ROOT/.devfactory.yml $THEIA_WORKSPACE_ROOT/.gitpod.yml
 cp $THEIA_WORKSPACE_ROOT/.devfactory.yml $THEIA_WORKSPACE_ROOT/.gitpod2.yml
