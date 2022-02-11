9ad3374f1432178ed996f67d5007118c96af404c
achrafabidatekab <achraf.abida@tekab.dev>
2022-02-11 11:09:51 +0000

    update execlude path

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 if [ -f $THEIA_WORKSPACE_ROOT/.devfactory.yml ]; then
     cp $THEIA_WORKSPACE_ROOT/.devfactory.yml $THEIA_WORKSPACE_ROOT/.gitpod.yml
 fi
-git config --global core.excludesFile '.gitexclude'
+git config --global core.excludesFile '/home/gitpod/.dotfiles/.gitexclude'
